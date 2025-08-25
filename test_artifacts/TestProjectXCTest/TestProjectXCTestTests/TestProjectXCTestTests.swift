c52f40a1cfdd0fed50e50df5a75e146f92577eed
Stefan <nitustefandragos@gmail.com>
2025-08-25 10:05:21 +0300

    Add test methods for filter testing: targetForFilterTest and failingTest

--- test_artifacts/TestProjectXCTest/TestProjectXCTestTests/TestProjectXCTestTests.swift
+++ test_artifacts/TestProjectXCTest/TestProjectXCTestTests/TestProjectXCTestTests.swift
@@ -31,5 +31,17 @@ final class TestProjectXCTestTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func targetForFilterTest() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func failingTest() throws {
+        XCTFail("Test MCP failing test reporting")
+    }
 
 }
